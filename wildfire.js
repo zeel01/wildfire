b70edff905c190eff8a40e1168c40ef7e082f4cf
zeel <zeel02@gmail.com>
2020-10-19 22:59:03 -0400

    Fixed incorrect boolean not causing no fires to spawn.

--- wildfire.js
+++ wildfire.js
@@ -120,7 +120,7 @@ class Wildfire {
 	isBurning(cell) {
 		const [x, y] = Wildfire.getPixelPos(cell);
 		const box = new PIXI.Rectangle(x, y, this.gridSize, this.gridSize);
-		return !this.fires.some(t => box.contains(t.x, t.y))
+		return this.fires.some(t => box.contains(t.x, t.y))
 	}
 
 	/**
@@ -132,6 +132,8 @@ class Wildfire {
 	*/
 	isFlamable(cell) {
 		if (this.isBurning(cell)) return false;
+
+		return true;
 	}
 
 	/**
@@ -213,34 +215,8 @@ class Wildfire {
 }
 
 async function spreadFire() {
-	/*	const fire = canvas.tokens.objects.children.filter(t => t.data.name == "Fire");
-		const newFires = [];
-	
-		for (let t of fire) {
-			const s = canvas.grid.grid.w;
-			const [r, c] = canvas.grid.grid.getGridPositionFromPixels(t.x, t.y);
-			const neighbors = canvas.grid.grid.getNeighbors(r, c);
-			const notBurning = neighbors.filter(n => {
-				const [x, y] = canvas.grid.grid.getPixelsFromGridPosition(n[0], n[1]);
-				const box = new PIXI.Rectangle(x, y, s, s);
-				return !canvas.tokens.objects.children.some(t =>
-					box.contains(t.x, t.y) && t.data.name == "Fire"
-				)
-			})
-			notBurning.forEach(s => {
-				if (new Roll("1d6").roll().total > 4) {
-					const [x, y] = canvas.grid.grid.getPixelsFromGridPosition(s[0], s[1]);
-					const tkn = duplicate(t.data);
-					tkn.x = x; tkn.y = y;
-					newFires.push(tkn);
-				}
-			})
-		}
-	
-		canvas.tokens.createMany(newFires);
-	*/
 	new Wildfire(_token).spread({
-		formula: "1d6",
-		success: 5
+		formula: "1d8",
+		success: 8
 	});
 }
